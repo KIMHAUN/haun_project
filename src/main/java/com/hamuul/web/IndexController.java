6dca1267a39e510206260e58c9be4a4efc1b80e7
김하은 <wha02068@gmail.com>
2022-09-21 11:23:50 +0900

    OAuth2 로그인하기

--- src/main/java/com/hamuul/web/IndexController.java
+++ src/main/java/com/hamuul/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hamuul.web;
 
+import com.hamuul.config.auth.dto.SessionUser;
 import com.hamuul.service.posts.PostsService;
 import com.hamuul.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
       @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
 
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); //CustomOAuth2UserService에서 로그인 성공시 세션에 SessionUser저장
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index"; //src/main/resources/templates/index.mustache
     }
 
