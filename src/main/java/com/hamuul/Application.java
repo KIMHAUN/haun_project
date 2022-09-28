b743fe0e2f298e049f61e6cd8fd1bc3c0bc8cc32
김하은 <wha02068@gmail.com>
2022-09-28 09:31:17 +0900

    5.7 기존 테스트에 시큐리티 적용하기

--- src/main/java/com/hamuul/Application.java
+++ src/main/java/com/hamuul/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing - WebMvcTest에는 최소 하나의 Entity가 없어서 에러.
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
